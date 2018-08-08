f08f40fe469351d0b1524ba603a58164bbc4ae7f
Anggun Pribadi <anxggxun@gmail.com>
2018-08-08 20:47:52 +0700

    use echo esc_html rather than esc_html_e to echo site description

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
 
 					$description = get_bloginfo( 'description', 'display' );
 					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php esc_html_e( $description ); ?></p>
+						<p class="site-description"><?php echo esc_html( $description ); ?></p>
 					<?php endif; ?>
 				</div><!-- .site-branding -->
 
